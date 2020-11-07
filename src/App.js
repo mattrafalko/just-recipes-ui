1762decfbee7137148fc75a3be625071ec8f036e
mattrafalko <mattrafalko@gmail.com>
2020-11-07 09:28:55 -0600

    removed userContextUpdate - you dont need a separate context for updating the value of another context

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Recipes from './components/Views/Recipes';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Views/Login';
-
-export const UserContext = React.createContext({});
-export const UserContextUpdate = React.createContext(() => {});
+import { UserContext } from './context/UserContext';
 
 function App() {
-  const [userId, setUserId] = useState(null);
+  const context_userId = useContext(UserContext);
+  const [userId, setUserId] = useState(context_userId);
+
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
-        <Header appName="Just Recipes" userId="6"></Header>
-          <UserContext.Provider value={userId}>
-            <UserContextUpdate.Provider value={setUserId}>
-              <Switch>
-                <Route exact path="/"><p>This is the home page</p></Route>
-                <Route path="/about"><p>This is the about page</p></Route>
-                <Route path="/recipes"><Recipes/></Route>
-                <Route path="/login"><Login/></Route>
-              </Switch>
-            </UserContextUpdate.Provider>
-          </UserContext.Provider>
+        <Header appName='Just Recipes' userId={userId}></Header>
+        <UserContext.Provider value={{ userId, setUserId }}>
+          <Switch>
+            <Route exact path='/'>
+              <p>This is the home page</p>
+            </Route>
+            <Route path='/about'>
+              <p>This is the about page</p>
+            </Route>
+            <Route path='/recipes'>
+              <Recipes />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+          </Switch>
+        </UserContext.Provider>
         <Footer></Footer>
       </Router>
     </div>
